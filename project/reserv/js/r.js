c1cf2c16d1b299bd9b7f8eaa39b7caee4e706403
kimss <niizguy@douzone.com>
2022-08-03 14:19:19 +0900

    예약확인 샘플.

---
+++ project/reserv/js/r.js
@@ -0,0 +1,73 @@
+
+var typeRoom = ['dbd','nbd','hhbd'];
+var dateInfo8 = ['2022-08-14','2022-08-20','2022-08-27'];
+var dateInfo9 = ['2022-09-03','2022-09-09','2022-09-17','2022-09-24'];
+var findText = "type=dbd&today=2022-09-06";
+var reservTry;
+
+
+$(function() {
+    scrapingData();
+
+    // const timer = setInterval(() => {
+    //     scrapingData();
+    // }, 300000); 
+    
+    // clearInterval(timer); 
+});
+
+function currentTime() {
+    var today = new Date();   
+
+    var hours = ('0' + today.getHours()).slice(-2); 
+    var minutes = ('0' + today.getMinutes()).slice(-2);
+    var seconds = ('0' + today.getSeconds()).slice(-2); 
+
+    var timeString = hours + ':' + minutes  + ':' + seconds;
+
+    return timeString;
+}
+function scrapingData() {
+    var revTitle = "";
+    $.get("https://www.campingkorea.or.kr/reservation/06.htm?code=&year=2022&month=8#container", function(data) {
+        result = data.match(/{/g);
+        
+
+        $.each(dateInfo8, function(index, item) {
+            for(var i=0; i <= typeRoom.length-1; i++) {
+                findText = 'type='+typeRoom[i]+'&today='+item;
+                reservTry = data.lastIndexOf(findText);
+                console.log(findText);
+                if(reservTry > 0) {
+                    $('.reservList').append('<li>' + findText + "</li>");
+                    revTitle += typeRoom[i]+item.substr(5)+" ";
+                }
+            }
+        });
+        
+    });
+
+    $.get("https://www.campingkorea.or.kr/reservation/06.htm?code=&year=2022&month=9#container", function(data) {
+        result = data.match(/{/g);
+
+        $.each(dateInfo9, function(index, item) {
+            for(var i=0; i <= typeRoom.length-1; i++) {
+                findText = 'type='+typeRoom[i]+'&today='+item;
+                reservTry = data.lastIndexOf(findText);
+                console.log(findText);
+                if(reservTry > 0) {
+                    $('.reservList').append('<li>' + findText + "</li>");
+                    revTitle += typeRoom[i]+item.substr(5)+" ";
+                }
+            }
+        });
+        if(revTitle != "") {
+            document.title = revTitle;
+        }
+        else {
+            document.title = currentTime();
+        }
+        
+    });
+    
+}
