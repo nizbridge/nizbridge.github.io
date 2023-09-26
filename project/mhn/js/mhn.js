8ee1cc5d3f54fd575d55b20b0e80fbde7cc7e605
kimss <niizguy@douzone.com>
2023-09-26 17:03:07 +0900

    fix

--- project/mhn/js/mhn.js
+++ project/mhn/js/mhn.js
@@ -1,81 +1,78 @@
 let testData = JSON.parse(JSON.stringify(TestFile));
 console.log(testData);
 
-const searchForm = document.getElementById("searchForm");
-const searchInput = document.getElementById("searchInput");
-const searchButton = document.getElementById("searchButton");
-const searchResult = document.getElementById("searchResult");
-
-// 스킬 및 장비 데이터 (예시)
-const skills = ["스킬 1", "스킬 2", "스킬 3"];
-const equipment = ["장비 1", "장비 2", "장비 3"];
-
-
 document.addEventListener("DOMContentLoaded", function () {
-    const simulationForm = document.getElementById("simulationForm");
-    const simulationResult = document.getElementById("simulationResult");
+    const selectedBodyPart = document.getElementById("selectedBodyPart");
+    const selectedBodyPartLabel = document.getElementById("selectedBodyPartLabel");
+    const selectedEquipment = document.getElementById("selectedEquipment");
+    const skillList = document.getElementById("skillList");
+    const equipmentSkills = document.getElementById("equipmentSkills");
+    const addBuildButton = document.getElementById("addBuildButton");
+    const buildList = document.getElementById("buildList");
 
-    simulationForm.addEventListener("submit", function (e) {
-        e.preventDefault();
+    // 부위별 장비 데이터 (예시)
+    const bodyPartEquipmentData = {
+        head: ["머리 장비 1", "머리 장비 2"],
+        torso: ["몸통 장비 1", "몸통 장비 2"],
+        chest: ["가슴 장비 1", "가슴 장비 2"],
+        // 다른 부위의 장비 목록 추가
+    };
 
-        const selectedSkill = document.getElementById("skill").value;
-        const selectedEquipment = document.getElementById("equipment").value;
+    // 부위 선택 변경 시 이벤트 핸들러
+    selectedBodyPart.addEventListener("change", function () {
+        const selected = selectedBodyPart.value;
+        const equipmentList = bodyPartEquipmentData[selected] || [];
 
-        // 여기에서 스킬 시뮬레이션 로직을 작성하고 결과를 계산합니다.
-        // 예를 들어, 선택된 스킬과 장비의 조합에 따른 시뮬레이션 결과를 계산할 수 있습니다.
-
-        // 결과를 출력
-        const resultHTML = `
-            <h2>스킬 시뮬레이션 결과</h2>
-            <p>선택된 스킬: ${selectedSkill}</p>
-            <p>선택된 장비: ${selectedEquipment}</p>
-            <!-- 시뮬레이션 결과를 여기에 추가 -->
-        `;
-
-        simulationResult.innerHTML = resultHTML;
+        // 선택한 부위에 따라 장비 목록을 업데이트
+        updateEquipmentList(equipmentList);
+        updateSelectedBodyPartLabel(selected);
     });
 
+    // 장비 목록 업데이트 함수
+    function updateEquipmentList(equipmentList) {
+        selectedEquipment.innerHTML = "";
 
+        equipmentList.forEach(equipment => {
+            const option = document.createElement("option");
+            option.value = equipment;
+            option.textContent = equipment;
+            selectedEquipment.appendChild(option);
+        });
+    }
 
-    // 검색 버튼 클릭 시
-    searchButton.addEventListener("click", function () {
-        const searchTerm = searchInput.value.toLowerCase(); // 검색어를 소문자로 변환
+    // 선택한 부위 레이블 업데이트 함수
+    function updateSelectedBodyPartLabel(selected) {
+        selectedBodyPartLabel.textContent = selected !== "" ? selected : "부위 없음";
+    }
 
-        // 스킬 및 장비 배열에서 검색어와 일치하는 요소 찾기
-        const matchingSkills = skills.filter(skill => skill.toLowerCase().includes(searchTerm));
-        const matchingEquipment = equipment.filter(equip => equip.toLowerCase().includes(searchTerm));
+    // 빌드 목록
+    const builds = [];
 
-        // 검색 결과 출력
-        displaySearchResults(matchingSkills, matchingEquipment);
+    // 빌드 추가 버튼 클릭 시
+    addBuildButton.addEventListener("click", function () {
+        const selectedPart = selectedBodyPart.value;
+        const selectedEquip = selectedEquipment.value;
+
+        // 새로운 빌드 객체 생성 및 배열에 추가
+        const build = {
+            bodyPart: selectedPart,
+            equipment: selectedEquip
+        };
+        builds.push(build);
+
+        // 빌드 목록 업데이트
+        updateBuildList();
     });
 
-    // 검색 결과 출력 함수
-    function displaySearchResults(skills, equipment) {
-        let resultHTML = "";
+    // 빌드 목록 업데이트 함수
+    function updateBuildList() {
+        buildList.innerHTML = "";
 
-        if (skills.length > 0) {
-            resultHTML += "<h2>일치하는 스킬:</h2>";
-            resultHTML += "<ul>";
-            skills.forEach(skill => {
-                resultHTML += `<li>${skill}</li>`;
-            });
-            resultHTML += "</ul>";
-        }
-
-        if (equipment.length > 0) {
-            resultHTML += "<h2>일치하는 장비:</h2>";
-            resultHTML += "<ul>";
-            equipment.forEach(equip => {
-                resultHTML += `<li>${equip}</li>`;
-            });
-            resultHTML += "</ul>";
-        }
-        else {
-            resultHTML += "<h2>일치하는 장비:</h2>";
-            resultHTML += "없습니다.";
-        }
-        
-        // 검색 결과를 화면에 표시
-        searchResult.innerHTML = resultHTML;
+        builds.forEach((build, index) => {
+            const listItem = document.createElement("li");
+            listItem.textContent = `빌드 ${index + 1}: 부위 - ${build.bodyPart}, 장비 - ${build.equipment}`;
+            buildList.appendChild(listItem);
+        });
     }
 });
+
