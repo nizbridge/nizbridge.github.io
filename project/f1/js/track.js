0a1bb55022f6abbd000914962c347a767d0e9f3d
kimss <niizguy@douzone.com>
2024-08-19 14:06:03 +0900

    fix

--- project/f1/js/track.js
+++ project/f1/js/track.js
@@ -1,71 +1,5 @@
 $(document).ready(function() {
-    const seasonFilePaths = {
-        'F3_S1': [
-            'data/f3_s1/event_200977_tier_1_results.csv', // Bahrain
-            'data/f3_s1/event_201216_tier_1_results.csv', // Saudi
-            'data/f3_s1/event_201215_tier_1_results.csv', // Australia
-            'data/f3_s1/event_201201_tier_1_results.csv', // Japan
-            'data/f3_s1/event_201213_tier_1_results.csv', // Miami
-            'data/f3_s1/event_201212_tier_1_results.csv', // Imola
-            'data/f3_s1/event_201209_tier_1_results.csv', // Canada
-            'data/f3_s1/event_201210_tier_1_results.csv', // Spain
-            'data/f3_s1/event_201208_tier_1_results.csv', // Autria
-            'data/f3_s1/event_201207_tier_1_results.csv', // Britain
-            'data/f3_s1/event_201206_tier_1_results.csv', // Hungary
-            'data/f3_s1/event_201205_tier_1_results.csv', // Belgium
-            'data/f3_s1/event_201204_tier_1_results.csv', // Netherlands
-            'data/f3_s1/event_201203_tier_1_results.csv', // Monza
-            'data/f3_s1/event_201214_tier_1_results.csv', // Azerbaijan
-            'data/f3_s1/event_201202_tier_1_results.csv', // Singapore
-            'data/f3_s1/event_201199_tier_1_results.csv', // Texax
-            'data/f3_s1/event_201127_tier_1_results.csv', // Mexico
-            'data/f3_s1/event_201126_tier_1_results.csv', // Brazil
-            'data/f3_s1/event_201125_tier_1_results.csv', // LasVegas
-            'data/f3_s1/event_201200_tier_1_results.csv', // Lusail
-            'data/f3_s1/event_201124_tier_1_results.csv', // AbuDhabi
-        ],
-        'F3_S2': [
-            'data/f3_s2/event_252300_tier_1_results.csv',
-            'data/f3_s2/event_252301_tier_1_results.csv',
-            'data/f3_s2/event_252302_tier_1_results.csv',
-            'data/f3_s2/event_252303_tier_1_results.csv',
-            'data/f3_s2/event_252304_tier_1_results.csv',
-            'data/f3_s2/event_252305_tier_1_results.csv',
-            'data/f3_s2/event_252306_tier_1_results.csv',
-            'data/f3_s2/event_252307_tier_1_results.csv',
-            'data/f3_s2/event_252308_tier_1_results.csv',
-            'data/f3_s2/event_252309_tier_1_results.csv',
-            'data/f3_s2/event_252310_tier_1_results.csv',
-            'data/f3_s2/event_252311_tier_1_results.csv',
-            'data/f3_s2/event_252312_tier_1_results.csv',
-            'data/f3_s2/event_252313_tier_1_results.csv',
-            'data/f3_s2/event_252314_tier_1_results.csv',
-            'data/f3_s2/event_252315_tier_1_results.csv',
-        ],
-        'F3_S3': [
-            'data/f3_s3/event_467172_tier_1_results.csv',
-            'data/f3_s3/event_467173_tier_1_results.csv',
-        ],
-        'F4_S1': [
-            'data/f4_s1/event_177632_tier_1_results.csv',
-            'data/f4_s1/event_177635_tier_1_results.csv',
-            'data/f4_s1/event_177636_tier_1_results.csv',
-            'data/f4_s1/event_177637_tier_1_results.csv',
-            'data/f4_s1/event_177638_tier_1_results.csv',
-            'data/f4_s1/event_177639_tier_1_results.csv',
-            'data/f4_s1/event_177640_tier_1_results.csv',
-            'data/f4_s1/event_177651_tier_1_results.csv',
-            'data/f4_s1/event_179913_tier_1_results.csv',
-        ],
-        'F4_S2': [
-            'data/f4_s2/event_199644_tier_1_results.csv',
-            'data/f4_s2/event_199645_tier_1_results.csv',
-        ],
-        'JJ_S1': [
-            'data/jj_s1/event_623042_tier_1_results.csv',
-            'data/jj_s1/event_623043_tier_1_results.csv',
-        ]
-    };
+    
 
     let allData = [];
 
@@ -287,7 +221,7 @@ $(document).ready(function() {
                 return true;
             }
         });
-        
+
         let tableData = sortedData.map((record, index) => {
             let fastestLapTime = formatTime(parseTime(record.FastestLap));
             let gap = index === 0 ? '' :
